91b186a8e1d894197ee45d03bd847aea2350f87e
zen-0wl <65055268+zen-0wl@users.noreply.github.com>
2024-06-04 21:08:59 +0800

    updates

---
+++ feature.js
@@ -0,0 +1,18 @@
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    content.style.display = content.style.display === "none" ? "block" : "none";
+}
+
+function showReport(reportId, insightId) {
+    var reports = document.querySelectorAll('.content');
+    var insights = document.querySelectorAll('.insights');
+    reports.forEach(report => report.style.display = 'none');
+    insights.forEach(insight => insight.style.display = 'none');
+    document.getElementById(reportId).style.display = 'block';
+    document.getElementById(insightId).style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    var contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.style.display = 'none');
+});

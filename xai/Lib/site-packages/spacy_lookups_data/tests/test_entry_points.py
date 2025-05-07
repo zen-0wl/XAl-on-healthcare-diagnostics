baf52da190c5775d4d24b2760b2763bb02af2b29
zen` <wintertemper479@gmail.com>
2025-05-08 03:26:01 +0800

    rm venv

--- xai/Lib/site-packages/spacy_lookups_data/tests/test_entry_points.py
+++
@@ -1,9 +0,0 @@
-import os
-import spacy
-
-
-def test_all_entry_points():
-    entry_points = spacy.registry.lookups.get_all()
-    for lang, tables in spacy.registry.lookups.get_all().items():
-        for name, filename in tables.items():
-            spacy.util.load_language_data(filename)
